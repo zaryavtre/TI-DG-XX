424a6529e5e735e39c641cadfbdc10ccbe2868a4
zaryavtre <von.zaryavtre@gmail.com>
2023-07-20 14:07:37 +0100

    log 001

--- dog.js
+++ dog.js
@@ -4,6 +4,26 @@ class Dog {
     constructor(data) {
         Object.assign(this, data)
     }
+    getDogeHtml() {
+        const {name, avatar, age, bio} = this
+        return `
+            <div class="main-dog">
+                <div class="dog-pic-wrapper">
+                    <img class="dog-pic" src="${avatar}">
+                    <div class="dog-stat-wrapper">
+                        <div class="dog-name-age">
+                            <h2 class="dog-name">${name}</h2>
+                            <span>,</span>
+                            <p class="dog-age">${age}</p>
+                        </div>
+                    <div class="bio-wrapper">
+                        <p class="dog-bio">${bio}</p>
+                        
+
+                </div>
+
+        `
+    }
 }
 
 
